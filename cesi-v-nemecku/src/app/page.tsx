e8498b44a31183385ce749b2d4c1ec0fbd2de718
Aneta Maňasová <aneta.manasova@email.cz>
2025-06-02 17:23:49 +0200

    feat: reworked to next.js

---
+++ cesi-v-nemecku/src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-white">
+      
+      <div className="bg-red-600 flex justify-center text-center">
+        <h1 className="bg-red-900 p-10 text-center font-bold text-6xl mt-16 mb-10 pl-44 pr-44 rounded-2xl">Češi v Německu</h1>
+      </div>
+      
+    </div>
+
+  );
+}
