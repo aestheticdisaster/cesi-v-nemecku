cb69f05fd513cfe86b4c1ce0161eacbc7488c52e
Aneta Maňasová <aneta.manasova@email.cz>
2025-06-05 17:11:16 +0200

    fix: downgrade tailwind to v3

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main>
       <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+      <link href="./output.css" rel="stylesheet"></link>
       <div className="text-6xl bg-amber-500">K 2.6.2025 byly řešeny technické problémy s GitHub Actions, obsah bude dodán v nejbližší možné době</div>
       <div className="min-h-screen">
         funguj
---
+++ out/_next/static/chunks/app/layout-ffbd466f2dc632c4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{2750:()=>{},3570:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,2750,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[41,60,145,358],()=>s(3570)),_N_E=e.O()}]);
