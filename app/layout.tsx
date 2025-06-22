d42774d49bfeb434a8080bec971377d7fbe9aeba
Aneta Maňasová <aneta.manasova@email.cz>
2025-06-22 17:58:40 +0200

    feat: add custom icon, add animations and new content

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,14 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: 'Češi v Německu',
   description: 'Češi v Německu',
+  icons: {
+  icon: [
+    {
+      url: "favicon.png",
+      href: "favicon.png",
+    },
+    ],
+  },
 };
 
 export default function RootLayout({
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,25 @@ module.exports = {
       backgroundImage: {
         'flag': "url('../public/Flag_of_Germany_(3-2).svg.png')",
         'wideFlag': "url('../public/Flag_of_Germany_(16-9).png')",
-      }
+        'munich': "url('../public/960px-Stadtbild_München.jpg')",
+        'lionflag': "url('../public/lionflag.png')"
+      },
+
+      animation: {
+        slideInLeft: 'slideInLeft 2s ease-out',
+        fadeIn: 'fadeIn 1s ease-in-out',
+      },
+
+      keyframes: {
+        slideInLeft: {
+          '0%': { transform: 'translateX(-10%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
     },
   },
   plugins: [],
