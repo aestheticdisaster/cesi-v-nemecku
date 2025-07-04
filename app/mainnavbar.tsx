1e8d905745661ab59612e559434f7e97070bc405
Aneta Maňasová <aneta.manasova@email.cz>
2025-07-04 14:24:55 +0200

    feat: optimize for small screens

--- app/mainnavbar.tsx
+++ app/mainnavbar.tsx
@@ -4,19 +4,19 @@ import OStrance from "./o-strance/page";
 
 export default function MainNavbar() {
     return(
-        <div className="bg-black flex items-center space-x-20 justify-between p-5 lg:px-20 h-20">
+        <div className="bg-black flex items-center lg:space-x-20 sm:space-x-10 justify-between p-5 lg:px-20 h-20">
 
             <div>
-                <h1 className="font-extrabold text-white text-3xl lg:text-4xl md:text-3xl sm:text-2xl transition-all duration-500">HKS 2025</h1>
+                <h1 className="font-extrabold text-white text-3xl lg:text-4xl sm:text-lg transition-all duration-500">HKS 2025</h1>
             </div>
 
             <div className="">
-                <Link href="/zakladni-informace" className="text-white font-extrabold hover:text-yellow-400 p-2 rounded-md text-md lg:text-xl md:text-lg sm:text-md transition-all duration-500 lg:m-7">Základní informace</Link>              
-                <Link href="/o-strance" className="text-white font-extrabold hover:text-yellow-400 p-2 text-md lg:text-xl md:text-lg sm:text-md transition-all duration-500 lg:m-7">O stránce</Link>
-                <Link href="/interaktivni-mapa" className="text-white font-extrabold hover:text-yellow-400 p-2 text-md lg:text-xl md:text-lg sm:text-md transition-all duration-500 lg:m-7">Interaktivní mapa</Link>
+                <Link href="/zakladni-informace" className="text-white font-extrabold hover:text-yellow-400 p-2 rounded-md text-md lg:text-xl sm:text-sm transition-all duration-500 lg:m-7">Základní informace</Link>              
+                <Link href="/o-strance" className="text-white font-extrabold hover:text-yellow-400 p-2 text-md lg:text-xl sm:text-sm transition-all duration-500 lg:m-7">O stránce</Link>
+                <Link href="/interaktivni-mapa" className="text-white font-extrabold hover:text-yellow-400 p-2 text-md lg:text-xl sm:text-sm transition-all duration-500 lg:m-7">Interaktivní mapa</Link>
             </div>
 
-            <div className="w-44"></div>
+            <div className="lg:w-44 sm:w-0"></div>
             
         </div>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -11,21 +11,21 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="bg-black">
-      <div className="text-xl p-2 bg-amber-500 text-center hover:text-white transition-all duration-500 hover:bg-amber-600">K 2.6.2025 byly řešeny technické problémy s GitHub Actions, obsah bude dodán v nejbližší možné době</div>
+      <div className="lg:text-xl sm:text-sm p-2 bg-amber-500 text-center hover:text-white transition-all duration-500 hover:bg-amber-600">K 2.6.2025 byly řešeny technické problémy s GitHub Actions, obsah bude dodán v nejbližší možné době</div>
     <MainNavbar />
     <div>
       <div className="relative min-h-screen bg-wideFlag flex items-center justify-start bg-center bg-cover bg-no-repeat scale-y-90 -mt-14">
         <div className="absolute inset-0 bg-black/20 bg-gradient-to-l from-black/0 to-black/90 z-0" />
-        <h1 className="relative text-7xl text-white font-extrabold p-5 pl-20 mt-48 scale-105 animate-slideInLeft">Češi v Německu</h1>
+        <h1 className="relative lg:text-7xl sm:text-4xl text-white font-extrabold p-5 lg:pl-20 sm:pl-10 mt-48 scale-105 animate-slideInLeft">Češi v Německu</h1>
       </div>
     </div>
     <div className="mt-20 min-h-screen">
       <div className="flex justify-center text-center">
-        <h1 className="text-white text-3xl p-5 animate-fadeIn">Češi v Německu jako minorita</h1>
+        <h1 className="text-white lg:text-3xl sm:text-2xl p-5 animate-fadeIn">Češi v Německu jako minorita</h1>
       </div>
-      <div className="flex justify-center text-right text-pretty space-x-10 max-w-6xl mx-auto p-5">
-        <p className="text-xl text-white p-5 animate-slideInLeft">Češi žijí v Německu v odhadovaném počtu 50 000 osob, přičemž nejvíce se zdržují s oblasti Bavorska, a to hlavně v okolí v Mnichova. Další skupiny se ale nachází i v Porýní-Vestfálsku, další menší skupiny v městských aglomeracích Hamburk, Berlín a další. V těchto a jiných oblastech se taktéž můžeme setkat s krajanskými spolky, čímž jsou různé školy, spolky všeho druhu, nebo i restaurace.</p>
-        <img src="960px-Stadtbild_München.jpg" width="480x240" className="rounded-xl animate-slideInLeft"/>
+      <div className="flex lg:flex-row sm:flex-col justify-center lg:text-right sm:text-center items-center text-pretty lg:space-x-10 sm:space-x-0 max-w-6xl mx-auto p-5">
+        <p className="text-xl text-white lg:p-5 sm:p-2 animate-slideInLeft">Češi žijí v Německu v odhadovaném počtu 50 000 osob, přičemž nejvíce se zdržují s oblasti Bavorska, a to hlavně v okolí v Mnichova. Další skupiny se ale nachází i v Porýní-Vestfálsku, další menší skupiny v městských aglomeracích Hamburk, Berlín a další. V těchto a jiných oblastech se taktéž můžeme setkat s krajanskými spolky, čímž jsou různé školy, spolky všeho druhu, nebo i restaurace.</p>
+        <img src="960px-Stadtbild_München.jpg" className="rounded-xl animate-slideInLeft lg:max-w-xl sm:max-w-md sm:mx-auto sm:justify-center"/>
       </div>
     </div>
     <Footer />
