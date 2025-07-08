0f336a740f844993503d8706577dfc15c52b888d
Aneta Maňasová <aneta.manasova@email.cz>
2025-07-08 17:40:47 +0200

    fix: remove sm prefix

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return(
         <footer className="fixed bottom-5 right-5">
-            <div className="text-white sm:text-xs">© Aneta Maňasová 2025</div>
+            <div className="text-white text-xs lg:text-lg">© Aneta Maňasová 2025</div>
         </footer>
     );
 }
